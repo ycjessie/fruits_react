53758c13645e2afebcf5ab57b4760dbcb739ecae
Jessie Chen <yhcjessie1@gmail.com>
2020-11-13 11:45:59 -0500

    search box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import ListContainer from './ListContainer';
+import Search from './Search';
 class App extends Component {
   render() { 
 
     return (
       <>
         <h1>Fruits</h1>
+        <Search />
         <ListContainer />
+        
       </>
     );
   }
---
+++ src/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class Search extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <label>Search Fruit </label>
+          <input type="text" placeholder="search for"></input>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Search;
