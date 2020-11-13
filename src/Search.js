011ec143abb471ad61447b77cb940bad8f3462a6
Jessie Chen <yhcjessie1@gmail.com>
2020-11-13 12:08:55 -0500

    add onChange event handler to Search component

--- src/Search.js
+++ src/Search.js
@@ -6,7 +6,8 @@ class Search extends Component {
         <div>
           <label>Search Fruit </label>
           <input type="text" placeholder="search for?"
-          value={this.props.value}></input>
+          value={this.props.value}
+          onChange={this.handleSearchChange}></input>
         </div>
       </>
     );
