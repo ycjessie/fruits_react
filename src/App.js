af2b472d1bd9ccdc1d80fc0396f2201b105d5a3a
Jessie Chen <yhcjessie1@gmail.com>
2020-11-13 10:51:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ListContainer from './ListContainer';
+class App extends Component {
+  render() { 
+
+    return (
+      <>
+        <h1>Fruits</h1>
+        <ListContainer />
+      </>
+    );
+  }
+}
+ 
+export default App;
+
---
+++ src/ListContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class ListContainer extends Component {
+    render() { 
+        return ( 
+
+            <ul>
+                <li>
+                    list items
+                </li>
+            </ul>
+
+         );
+    }
+}
+ 
+export default ListContainer;
