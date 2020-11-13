01e65acd309625593845a17708a799a80423d9ca
Jessie Chen <yhcjessie1@gmail.com>
2020-11-13 11:37:21 -0500

    seeddata in ListContainer use map() to show in HTML

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import ListItem from './ListItem';
+import React, { Component } from "react";
+import ListItem from "./ListItem";
+const Fruits = [
+  "Mango",
+  "Banana",
+  "Leechi",
+  "Orange",
+  "Apple",
+  "Pineapple",
+  "Dates",
+  "Grapes",
+];
 class ListContainer extends Component {
-    render() { 
-        return ( 
-
-            <ul>
-                <li>
-                    <ListItem name={"fake items"}/>
-                    <ListItem name={"fake items"}/>
-                    <ListItem name={"fake items"}/>
-                </li>
-            </ul>
-
-         );
-    }
+  render() {
+    const allFruits = Fruits.map((fruit, index) => {
+      return <ListItem name={fruit} key={index} />;
+    });
+    return (
+      <ul>
+        <li>{allFruits}</li>
+      </ul>
+    );
+  }
 }
- 
+
 export default ListContainer;
